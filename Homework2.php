6a972bc4b9d65fd307ae9668e1ece2812468f7c8
Vad008 <Vad008@ukr.net>
2015-10-15 20:54:17 +0300

    midle commit

--- Homework2.php
+++ Homework2.php
@@ -6,5 +6,23 @@
  * Time: 19:04
  * Homework2
  */
-$i = rand (1,8);
-echo $i;
+class number{
+    public $i = 0;
+    public $o = 0;
+    function create_number()
+    {
+        $this->i = rand (1,8);
+    }
+    function finding()
+    {
+        echo $this->i;
+        $this->o=$this->i%2;
+        echo $this->o;
+        if($this->o==0){
+
+        }
+    }
+}
+$number = new number();
+$number->create_number();
+$number->finding();
